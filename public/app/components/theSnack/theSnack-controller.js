024b26b743b92a42970d22f4c422206b07c211dd
Joel Schwartz <nederlands18@gmail.com>
2018-11-30 18:59:13 -0700

    push

---
+++ public/app/components/theSnack/theSnack-controller.js
@@ -0,0 +1,10 @@
+import TheSnackService from "./theSnack-Service.js"
+
+
+let _tss = new TheSnackService()
+
+export default class TheSnackController {
+  constructor() {
+
+  }
+}
--- public/app/main.js
+++ public/app/main.js
@@ -1,6 +1,7 @@
 import AuthService from "./components/auth/auth-service.js";
 import AuthController from "./components/auth/auth-controller.js"
 import SnackController from "./components/snack/snack-controller.js"
+import TheSnackController from "./components/theSnack/theSnack-controller.js"
 
 let auth = new AuthService()
 
@@ -10,7 +11,8 @@ class App {
   constructor() {
     this.controllers = {
       authController: new AuthController(auth),
-      snackController: new SnackController(auth)
+      snackController: new SnackController(auth),
+      theSnackController: new TheSnackController(auth)
 
     }
   }
